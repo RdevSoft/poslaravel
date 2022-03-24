9dad239b9a61db0f9eb89b178b2c9f5cced424ad
rgrammer <r.programmer.m@gmail.com>
2022-03-24 16:51:30 -0300

    reparacion component.blade.php POS

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -1,23 +1,30 @@
 <div>
     <style></style>
 
-    <div class="row-layout"></div>
+    <div class="row layout-top-spacing">
 
-    <div class="col-sm-12 col-md-8">
-        <!--Detalles-->
-        @include('livewire.pos.partials.detail')
-    </div>
+        <div class="col-sm-12 col-md-8">
+            <!--Detalles-->
+            @include('livewire.pos.partials.detail')
+        </div>
 
-    <div class="col-sm-12 col-md-4">
-        <!--TOTAl-->
-    @include('livewire.pos.partials.total')
+        <div class="col-sm-12 col-md-4">
+            <!--TOTAl-->
+        @include('livewire.pos.partials.total')
 
-    <!--Denomination/Coins-->
-        @include('livewire.pos.partials.coins')
+        <!--Denomination/Coins-->
+            @include('livewire.pos.partials.coins')
+        </div>
     </div>
 </div>
 
+<script src="{{ asset('asset/js/keypress.js') }}"></script>
+<script src="{{ asset('assets/js/onscan.js') }}"></script>
 
 <script>
 
 </script>
+
+@include('livewire.pos.scripts.shortcuts')
+@include('livewire.pos.scripts.events')
+@include('livewire.pos.scripts.general')
